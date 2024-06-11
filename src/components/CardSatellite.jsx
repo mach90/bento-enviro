6215ef2a7e78ef7c3b255bcf9ad244159b0a1994
mach90 <132974650+mach90@users.noreply.github.com>
2024-06-11 13:10:22 +0200

    before refactoring

--- src/components/CardSatellite.jsx
+++ src/components/CardSatellite.jsx
@@ -1,7 +1,9 @@
+import satelliteImage from '../img/satellite.jpg';
+
 export default function CardSatellite() {
     return (
         <div className="lg:row-span-2 text-stone-700 p-1 font-custom1 font-700 text-2xl">
-            <img src="src/img/satellite.jpg" alt="satimage"className="rounded-3xl" width="100%" height="100%" />
+            <img src={satelliteImage} alt="satimage"className="rounded-3xl" width="100%" height="100%" />
         </div>
     );
 }
