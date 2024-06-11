6b279770b36f17fa152b5945f77fe48bb46aa7a0
mach90 <132974650+mach90@users.noreply.github.com>
2024-06-11 13:57:14 +0200

    before refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ export default function App() {
           <CardForecast />
         </ForecastProvider>
 
-        {/* <CardMoon /> */}
+        <CardMoon />
 
         <MoonProvider>
           <CardMoon />
--- src/components/CardMoon.jsx
+++ src/components/CardMoon.jsx
@@ -3,9 +3,8 @@ import { useMoon } from "../context/moonContext";
 export default function CardMoon() {
     const {moonAge, moonPhaseAngle, moonPhaseName,moonPhaseTransitTime, moonrise, moonset} = useMoon();
 
-    // const moonAge = 12; 
-    // // const moonIlluminatedFraction = 90; 
-    // const moonPhaseAngle = 1; 
+    // const moonAge = 0; 
+    // const moonPhaseAngle = 0; 
     // const moonPhaseName = "Something crescent";
     // const moonPhaseTransitTime = "12:34"; 
     // const moonrise = "12:34"; 
@@ -17,12 +16,28 @@ export default function CardMoon() {
             <div className="flex flex-col justify-center items-center absolute ">
                 <p className="bg-stone-800 bg-opacity-50 p-1 rounded-xl">Moonrise: {moonrise ? moonrise : "??:??"}</p>
                 <p className="bg-stone-800 bg-opacity-50 p-1 rounded-xl">Moonset: {moonset ? moonset : "??:??"}</p>
-                <p className="bg-stone-800 bg-opacity-50 p-1 rounded-xl">{moonPhaseName ? moonPhaseName : "unknown"} ({moonPhaseAngle ? moonPhaseAngle : "unknown"}%)</p>
+                <p className="bg-stone-800 bg-opacity-50 p-1 rounded-xl">{moonPhaseName ? moonPhaseName : "unknown"} ({moonPhaseAngle >= 0 ? moonPhaseAngle : "unknown"}%)</p>
                 <p className="bg-stone-800 bg-opacity-50 p-1 rounded-xl">Transit: {moonPhaseTransitTime ? moonPhaseTransitTime : "??:??"}</p>
-                <p className="bg-stone-800 bg-opacity-50 p-1 rounded-xl">{moonAge ? `${moonAge} days` : "unknown"}</p>
+                <p className="bg-stone-800 bg-opacity-50 p-1 rounded-xl">{moonAge >= 0 ? `${moonAge} days` : "unknown"}</p>
             </div>
 
-            {moonPhaseAngle > 50 &&  
+            {moonPhaseAngle <= 50 && moonAge < 10 && 
+            <svg width="250" height="250" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <g transform="rotate(0, 50, 50)">
+                    <circle cx="50" cy="50" r="50" fill="#303030"/>
+                    <path id="crescent" fill="#d1d1d1" d={`M 50 0 A 50 50 0 1 1 50 100 A ${50 - moonPhaseAngle} 50 0 1 0 50 0 Z`}/>
+                </g>
+            </svg>}
+
+            {moonPhaseAngle > 50 && moonAge < 16 && 
+            <svg width="250" height="250" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <g transform="rotate(180, 50, 50)">
+                    <circle cx="50" cy="50" r="50" fill="#d1d1d1"/>
+                    <path id="crescent" fill="#303030" d={`M 50 0 A 50 50 0 1 1 50 100 A ${moonPhaseAngle - 50} 50 0 1 0 50 0 Z`}/>
+                </g>
+            </svg>}
+
+            {moonPhaseAngle > 50 && moonAge > 15 && 
             <svg width="250" height="250" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
                 <g transform="rotate(0, 50, 50)">
                     <circle cx="50" cy="50" r="50" fill="#d1d1d1"/>
@@ -30,11 +45,11 @@ export default function CardMoon() {
                 </g>
             </svg>}
 
-            {moonPhaseAngle <= 50 &&
+            {moonPhaseAngle <= 50 && moonAge > 21 && 
             <svg width="250" height="250" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
                 <g transform="rotate(180, 50, 50)">
                     <circle cx="50" cy="50" r="50" fill="#303030"/>
-                    <path id="crescent" fill="#d1d1d1" d={`M 50 0 A 50 50 0 1 1 50 100 A ${50 - moonPhaseAngle} 50 0 1 0 50 0 Z`}/>
+                    <path id="crescent" fill="#d1d1d1" d={`M 50 0 A 50 50 0 1 1 50 100 A ${moonPhaseAngle - 50} 50 0 1 0 50 0 Z`}/>
                 </g>
             </svg>}
 
