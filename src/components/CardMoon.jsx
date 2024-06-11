8551162db8738abeb111d4bc07af2138d273dde0
mach90 <132974650+mach90@users.noreply.github.com>
2024-06-11 14:50:45 +0200

    before refactoring

--- src/components/CardMoon.jsx
+++ src/components/CardMoon.jsx
@@ -1,11 +1,12 @@
-import { useMoon } from "../context/moonContext";
+import { useMoon } from "../context/moonContext";s
 
 export default function CardMoon() {
-    const {moonAge, moonPhaseAngle, moonPhaseName,moonPhaseTransitTime, moonrise, moonset} = useMoon();
+    const {moonAge, moonIlluminatedFraction, moonPhaseName,moonPhaseTransitTime, moonrise, moonset} = useMoon();
 
-    // const moonAge = 0; 
-    // const moonPhaseAngle = 0; 
-    // const moonPhaseName = "Something crescent";
+    // const moonAge = 4; 
+    // // const moonPhaseAngle = 50;
+    // const moonIlluminatedFraction = 65; 
+    // const moonPhaseName = "waning crescent";
     // const moonPhaseTransitTime = "12:34"; 
     // const moonrise = "12:34"; 
     // const moonset = "12:34";
@@ -16,40 +17,72 @@ export default function CardMoon() {
             <div className="flex flex-col justify-center items-center absolute ">
                 <p className="bg-stone-800 bg-opacity-50 p-1 rounded-xl">Moonrise: {moonrise ? moonrise : "??:??"}</p>
                 <p className="bg-stone-800 bg-opacity-50 p-1 rounded-xl">Moonset: {moonset ? moonset : "??:??"}</p>
-                <p className="bg-stone-800 bg-opacity-50 p-1 rounded-xl">{moonPhaseName ? moonPhaseName : "unknown"} ({moonPhaseAngle >= 0 ? moonPhaseAngle : "unknown"}%)</p>
+                <p className="bg-stone-800 bg-opacity-50 p-1 rounded-xl">{moonPhaseName ? moonPhaseName : "unknown"} ({moonIlluminatedFraction >= 0 ? moonIlluminatedFraction : "unknown"}%)</p>
                 <p className="bg-stone-800 bg-opacity-50 p-1 rounded-xl">Transit: {moonPhaseTransitTime ? moonPhaseTransitTime : "??:??"}</p>
                 <p className="bg-stone-800 bg-opacity-50 p-1 rounded-xl">{moonAge >= 0 ? `${moonAge} days` : "unknown"}</p>
             </div>
 
-            {moonPhaseAngle <= 50 && moonAge < 10 && 
+            {moonPhaseName === "new" && 
             <svg width="250" height="250" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
                 <g transform="rotate(0, 50, 50)">
                     <circle cx="50" cy="50" r="50" fill="#303030"/>
-                    <path id="crescent" fill="#d1d1d1" d={`M 50 0 A 50 50 0 1 1 50 100 A ${50 - moonPhaseAngle} 50 0 1 0 50 0 Z`}/>
+                    <path id="crescent" fill="#d1d1d1" d={`M 50 0 A 50 50 0 1 1 50 100 A ${50 - moonIlluminatedFraction} 50 0 1 0 50 0 Z`}/>
                 </g>
             </svg>}
 
-            {moonPhaseAngle > 50 && moonAge < 16 && 
+            {moonPhaseName === "waxing crescent" && 
+            <svg width="250" height="250" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <g transform="rotate(0, 50, 50)">
+                    <circle cx="50" cy="50" r="50" fill="#303030"/>
+                    <path id="crescent" fill="#d1d1d1" d={`M 50 0 A 50 50 0 1 1 50 100 A ${50 - moonIlluminatedFraction} 50 0 1 0 50 0 Z`}/>
+                </g>
+            </svg>}
+
+            {moonPhaseName === "first quarter" && 
+            <svg width="250" height="250" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <g transform="rotate(0, 50, 50)">
+                    <circle cx="50" cy="50" r="50" fill="#303030"/>
+                    <path id="crescent" fill="#d1d1d1" d={`M 50 0 A 50 50 0 1 1 50 100 A ${50 - moonIlluminatedFraction} 50 0 1 0 50 0 Z`}/>
+                </g>
+            </svg>}
+
+            {moonPhaseName === "waxing gibbous" && 
             <svg width="250" height="250" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
                 <g transform="rotate(180, 50, 50)">
                     <circle cx="50" cy="50" r="50" fill="#d1d1d1"/>
-                    <path id="crescent" fill="#303030" d={`M 50 0 A 50 50 0 1 1 50 100 A ${moonPhaseAngle - 50} 50 0 1 0 50 0 Z`}/>
+                    <path id="crescent" fill="#303030" d={`M 50 0 A 50 50 0 1 1 50 100 A ${50 - moonIlluminatedFraction} 50 0 1 0 50 0 Z`}/>
                 </g>
             </svg>}
 
-            {moonPhaseAngle > 50 && moonAge > 15 && 
+            {moonPhaseName === "full" && 
+            <svg width="250" height="250" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <g transform="rotate(180, 50, 50)">
+                    <circle cx="50" cy="50" r="50" fill="#d1d1d1"/>
+                    <path id="crescent" fill="#303030" d={`M 50 0 A 50 50 0 1 1 50 100 A ${50 - moonIlluminatedFraction} 50 0 1 0 50 0 Z`}/>
+                </g>
+            </svg>}
+
+            {moonPhaseName === "waning gibbous" && 
             <svg width="250" height="250" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
                 <g transform="rotate(0, 50, 50)">
                     <circle cx="50" cy="50" r="50" fill="#d1d1d1"/>
-                    <path id="crescent" fill="#303030" d={`M 50 0 A 50 50 0 1 1 50 100 A ${moonPhaseAngle - 50} 50 0 1 0 50 0 Z`}/>
+                    <path id="crescent" fill="#303030" d={`M 50 0 A 50 50 0 1 1 50 100 A ${50 - moonIlluminatedFraction} 50 0 1 0 50 0 Z`}/>
                 </g>
             </svg>}
 
-            {moonPhaseAngle <= 50 && moonAge > 21 && 
+            {moonPhaseName === "last quarter" && 
             <svg width="250" height="250" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
                 <g transform="rotate(180, 50, 50)">
                     <circle cx="50" cy="50" r="50" fill="#303030"/>
-                    <path id="crescent" fill="#d1d1d1" d={`M 50 0 A 50 50 0 1 1 50 100 A ${moonPhaseAngle - 50} 50 0 1 0 50 0 Z`}/>
+                    <path id="crescent" fill="#d1d1d1" d={`M 50 0 A 50 50 0 1 1 50 100 A ${50 - moonIlluminatedFraction} 50 0 1 0 50 0 Z`}/>
+                </g>
+            </svg>}
+
+            {moonPhaseName === "waning crescent" && 
+            <svg width="250" height="250" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <g transform="rotate(180, 50, 50)">
+                    <circle cx="50" cy="50" r="50" fill="#303030"/>
+                    <path id="crescent" fill="#d1d1d1" d={`M 50 0 A 50 50 0 1 1 50 100 A ${50 - moonIlluminatedFraction} 50 0 1 0 50 0 Z`}/>
                 </g>
             </svg>}
 
